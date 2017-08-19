63eaed58ef061e567033739c6f502e95b4238ec9
Dmitry <nor808@yandex.ru>
2017-08-19 14:00:03 +0300

    test commit

--- CheckiO.py
+++ CheckiO.py
@@ -1,19 +1,5 @@
-def long_repeat(line):
-    count = 1
-    max = 1
-    print(line)
-    letter = ""
-    for i in range(len(line)-1):
-        if line[i] == line[i+1]:
-            count += 1
-            letter = line[i]
-        if line[i] != line[i+1] and line[i] != letter:
-            count = 1
-        if max < count:
-            max = count
-    if line:
-        return max
-    else:
-        return 0
+def checkio(expression):
+    # return True or False
+    return 0
 
-print(long_repeat('ddvvrwwwrggg'))
+print(checkio("((5+3)*2+1)"))
